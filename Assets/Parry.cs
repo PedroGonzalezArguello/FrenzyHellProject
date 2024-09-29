297dd2a1bc635023f7a22a154ee2909e462753a4
AxelVLee <axel.vernik@davinci.edu.ar>
2024-09-28 22:57:26 -0300

    parry

--- Assets/Parry.cs
+++ Assets/Parry.cs
@@ -3,21 +3,27 @@ using UnityEngine.UI;
 
 public class Parry : MonoBehaviour
 {
-    private float _parryTime = 0.3f;
+    private float _parryTime = 0.1f;
     [SerializeField] private float _timer;
     [SerializeField] private bool _inParry;
+    [SerializeField] private GameObject _parryCube;
 
-
+    private void Awake()
+    {
+        _parryCube.SetActive(false);
+    }
     private void Update()
     {
         if((Input.GetButton("ParryKey")))
         {
             _timer = _parryTime;
+            _parryCube.SetActive(true);
         }
         _timer -= Time.deltaTime;
         if (_timer <= 0)
         {
             _inParry = false;
+            _parryCube.SetActive(false);
         }
         else
         {
@@ -31,7 +37,8 @@ public class Parry : MonoBehaviour
 
         if (parryable != null && _inParry)
         {
-            StartCoroutine(parryable.Parry()); 
+            StartCoroutine(parryable.Parry());
+            _parryCube.SetActive(true);
         }
     }
 }
